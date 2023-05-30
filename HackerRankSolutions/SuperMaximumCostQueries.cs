6f02e77a885e4136e0d6b35835b3aaf401cca437
ssaporito <sandrosaporito@outlook.com>
2023-05-30 00:43:15 -0300

    [SuperMaximumCostQueries] First attempts

---
+++ HackerRankSolutions/SuperMaximumCostQueries.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.SuperMaximumCostQueries
+{
+    public class SuperMaximumCostQueries
+    {
+        public static List<int> Solve(int n, List<List<int>> tree, List<List<int>> queries)
+        {
+            var sortedTree = tree.OrderBy(adj => adj[2]).ToList();
+            return queries.Select(q => PathCount(n, sortedTree, q[0], q[1])).ToList();
+        }
+
+        public static int PathCount(int n, List<List<int>> sortedTree, int l, int r)
+        {
+            var allowedSubtree = sortedTree.Where(adj => adj[2] >= l && adj[2] <= r).ToList();
+            var adjacencyList = TreeToAdjacencyList(allowedSubtree);
+            var connectedComponents = ConnectedComponents(adjacencyList);                    
+            var result = connectedComponents.Sum(x => ComponentPathCount(x));
+            return result;
+        }
+        
+        public static int ComponentPathCount(List<int> component)
+        {
+            int n = component.Count;
+            return n * (n - 1) / 2;
+        }
+
+        public static List<List<int>> ConnectedComponents(Dictionary<int, HashSet<int>> adjacencyList)
+        {
+            var remainingNodes = new HashSet<int>(adjacencyList.Keys);
+            var components = new List<List<int>>(); 
+            
+            while (remainingNodes.Any())
+            {
+                var component = new List<int>();
+                var explored = new HashSet<int>();
+                var stack = new Stack<int>();
+                var firstNodeInComponent = remainingNodes.First();
+                stack.Push(firstNodeInComponent);
+
+                while (stack.Any())
+                {                    
+                    int u = stack.Pop();
+                    if (explored.Contains(u))
+                        continue;
+                    
+                    component.Add(u);
+                    remainingNodes.Remove(u);
+
+                    var neighbors = adjacencyList[u];
+                    foreach (var neighbor in neighbors)
+                    {
+                        if (explored.Contains(neighbor))
+                            continue;
+
+                        stack.Push(neighbor);
+                    }
+                    
+                    explored.Add(u);
+                }
+                components.Add(component);
+            }
+            return components;
+        }
+
+        public static Dictionary<int, HashSet<int>> TreeToAdjacencyList(List<List<int>> tree)
+        {
+            Dictionary<int, HashSet<int>> adjList = new();
+            foreach (var adj in tree)
+            {
+                int u = adj[0];
+                int v = adj[1];
+                if (!adjList.ContainsKey(u))
+                    adjList[u] = new HashSet<int>();
+                if (!adjList.ContainsKey(v))
+                    adjList[v] = new HashSet<int>();
+
+                adjList[u].Add(v);
+                adjList[v].Add(u);
+            }
+            return adjList;
+        }
+
+        public static Dictionary<int, Dictionary<int, int>> TreeToWeightedAdjacencyList(List<List<int>> tree)
+        {
+            Dictionary<int, Dictionary<int, int>> adjList = new();            
+            foreach(var adj in tree)
+            {
+                int u = adj[0];
+                int v = adj[1];
+                int w = adj[2];
+                if (!adjList.ContainsKey(u))
+                    adjList[u] = new Dictionary<int, int>();
+                if (!adjList.ContainsKey(v))
+                    adjList[v] = new Dictionary<int, int>();
+
+                adjList[u][v] = w;
+                adjList[v][u] = w;
+            }
+            return adjList;
+        }
+    }
+}
---
+++ HackerRankSolutionsTest/SuperMaximumCostQueriesTest.cs
@@ -0,0 +1,57 @@
+﻿using HackerRankSolutions.SuperMaximumCostQueries;
+using System.Text;
+
+namespace HackerRankSolutionsTest
+{
+    public class SuperMaximumCostQueriesTest
+    {
+        [Theory]
+        [PlainTextData(typeof(SuperMaximumCostQueriesCaseConverter))]
+        public void TestByFiles(int n, string treeString, string queriesString, string expectedString)
+        {
+            var tree = treeString.Split("\r\n").Select(line => line.Split(' ').Select(s => Convert.ToInt32(s)).ToList()).ToList();
+            var queries = queriesString.Split("\r\n").Select(line => line.Split(' ').Select(s => Convert.ToInt32(s)).ToList()).ToList();
+            var expectedList = expectedString.Split("\r\n").Select(line => Convert.ToInt32(line)).ToList();
+            var actualList = SuperMaximumCostQueries.Solve(n, tree, queries);
+            var range = Enumerable.Range(0, expectedList.Count);
+            var checks = range.Select(i => (Action)(() => Assert.Equal(expectedList[i], actualList[i]))).ToArray(); 
+            Assert.Multiple(checks);            
+        }
+
+        public class SuperMaximumCostQueriesCaseConverter : FileToTestCaseConverter
+        {
+            public override object[] ConvertInput(StreamReader inputStream)
+            {
+                string[] firstMultipleInput = inputStream.ReadLine().TrimEnd().Split(' ');
+                int n = Convert.ToInt32(firstMultipleInput[0]);
+                int q = Convert.ToInt32(firstMultipleInput[1]);
+
+                var sbTree = new StringBuilder();                
+                for (int i = 0; i < n - 1; i++)
+                {
+                    sbTree.Append((i != 0 ? "\r\n" : "") + inputStream.ReadLine().TrimEnd());                    
+                }
+
+                var sbQueries = new StringBuilder();
+                for (int i = 0; i < q; i++)
+                {
+                    sbQueries.Append((i != 0 ? "\r\n" : "") + inputStream.ReadLine().TrimEnd());
+                }
+
+                return new object[] { n, sbTree.ToString(), sbQueries.ToString() };
+            }
+
+            public override object ConvertOutput(StreamReader outputStream)
+            {                
+                var sbExpected = new StringBuilder();
+                string line;
+                int i = 0;
+                while ((line = outputStream.ReadLine()) != null) {
+                    sbExpected.Append((i != 0 ? "\r\n" : "") + line.TrimEnd());
+                    i++;
+                }
+                return sbExpected.ToString();
+            }
+        }
+    }
+}
