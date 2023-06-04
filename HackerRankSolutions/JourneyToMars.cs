6aae14679f02b836b2250d0558945a6bfc2b85bc
ssaporito <sandrosaporito@outlook.com>
2023-06-03 21:36:37 -0300

    [JourneyToMars] Working solution

---
+++ HackerRankSolutions/JourneyToMars.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.Numerics;
+
+namespace HackerRankSolutions.JourneyToMars
+{
+    public class JourneyToMars
+    {        
+        static readonly decimal _logTwoBaseTen = Convert.ToDecimal("0.30102999566398119521373889472449", CultureInfo.InvariantCulture);
+        public static int Solve(int n, int k)
+        {
+            var firstK = FirstKPowerOfTwo(n - 1, k);
+            var lastK = LastKPowerOfTwo(n - 1, k);
+            return firstK + lastK;
+        }
+
+        private static int FirstKPowerOfTwo(int n, int k)
+        {
+            var logTen = _logTwoBaseTen * n;
+            var nDigitCount = (int)Math.Ceiling(logTen);
+            var expo = logTen - nDigitCount + k;
+            var result = (int)Math.Pow(10, (double)expo);
+            return result;
+        }
+
+        private static int LastKPowerOfTwo(int n, int k)
+        {
+            var result = (int)BigInteger.ModPow(2, n, BigInteger.Pow(10, k));            
+            return result;
+        }
+    }
+}
---
+++ HackerRankSolutionsTest/JourneyToMarsTest.cs
@@ -0,0 +1,37 @@
+﻿using HackerRankSolutions.BiggerIsGreater;
+using HackerRankSolutions.JourneyToMars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutionsTest
+{
+    public class JourneyToMarsTest
+    {
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public static void TestSome(int n, int k, int expected)
+        {
+            var actual = JourneyToMars.Solve(n, k);
+            Assert.Equal(expected, actual);
+        }
+
+        public static IEnumerable<object[]> TestData =>
+            new List<object[]>
+            {
+                new object[] { 10, 2, 63 },
+                new object[] { 12, 1, 10 },
+                new object[] { 10000, 1, 17 },
+                new object[] { 10000, 2, 187 },
+                new object[] { 10000, 3, 1685 },
+                new object[] { 10000, 4, 14663 },
+                new object[] { 10000, 5, 154441 },
+                new object[] { 846930986, 2, 87 },
+                new object[] { 814637015, 1, 10 },
+                new object[] { 424238435, 6, 1731348 },
+                new object[] { 683276288, 8, 78277603 }
+            };
+    }
+}
