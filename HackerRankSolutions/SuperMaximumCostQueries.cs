81dadadd6ea917beab803e68361c3625271470d2
ssaporito <sandrosaporito@outlook.com>
2023-06-05 21:47:55 -0300

    [SuperMaximumCostQueries] Adding memoization

--- HackerRankSolutions/SuperMaximumCostQueries.cs
+++ HackerRankSolutions/SuperMaximumCostQueries.cs
@@ -10,7 +10,10 @@ using System.Threading.Tasks;
 namespace HackerRankSolutions.SuperMaximumCostQueries
 {
     public class SuperMaximumCostQueries
-    {       
+    {
+        private static readonly Dictionary<(int l, int r), QueryResult> _memo = new();
+        private record QueryResult(List<HashSet<int>> Components, List<long> PathCounts, Dictionary<int, int> NodeLabels);        
+
         public static List<long> Solve(int n, List<List<int>> tree, List<List<int>> queries)
         {
             var edges = tree.Select(adj => new Edge(adj[0] - 1, adj[1] - 1, adj[2])).ToList();
@@ -18,7 +21,7 @@ namespace HackerRankSolutions.SuperMaximumCostQueries
             return queries.Select(q => PathCount(edges, adjacencies, q[0], q[1])).ToList();
         }
 
-        private record struct Edge(int U, int V, int W)
+        private record struct Edge(int U, int V, int W, int Component = -1)
         {
             public bool IsCentral(int l, int r) => W >= l && W <= r;
             public bool IsAllowed(int r) => W <= r;
@@ -37,21 +40,52 @@ namespace HackerRankSolutions.SuperMaximumCostQueries
         {
             return w < l;
         }
-
+        
         private static long PathCount(List<Edge> edges, List<List<(int node, int weight)>> adjacencies, int l, int r)
-        {            
-            long count = 0;
-            var centralNodes = edges.Where(e => e.IsCentral(l, r)).Select(e => new int[2] { e.U, e.V }).SelectMany(e => e).Distinct().ToList();
-            var explored = new HashSet<int>();            
-            var initialIndices = centralNodes;
+        {
+            try
+            {
+                var previousPair = _memo.Keys.First(query => query.l == l && query.r < r);
+                var queryResult  = _memo[previousPair];
+                var newEdges = edges.Where(e => e.W > previousPair.r && e.W <= r).ToList();
+                foreach (var newEdge in newEdges)
+                {
+                    var edgeNeighbors = adjacencies[newEdge.U].Union(adjacencies[newEdge.V]);
+                    var neighbors = edgeNeighbors.Where(e => e.weight >= previousPair.l && e.weight <= previousPair.r).Select(e => e.node);
+                    var firstLabeledNeighbor = neighbors.First(v => queryResult.NodeLabels.ContainsKey(v));
+                    var label = queryResult.NodeLabels[firstLabeledNeighbor];
+                    var connectedComponent = queryResult.Components[label];
+                    var nodeToAdd = newEdge.U == firstLabeledNeighbor ? newEdge.V : newEdge.U;
+                    connectedComponent.Add(nodeToAdd);
+                    queryResult.PathCounts[label] += connectedComponent.Count - 1;
+                    queryResult.NodeLabels[nodeToAdd] = label;                    
+                }
+                _memo[(l, r)] = queryResult;
+            }
+            catch
+            {
+                var centralNodes = edges.Where(e => e.IsCentral(l, r)).Select(e => new int[2] { e.U, e.V }).SelectMany(e => e).Distinct().ToList();
+                _memo[(l, r)] = GetComponents(centralNodes, adjacencies, l, r);                
+            }
 
+            return _memo[(l, r)].PathCounts.Sum();
+        }
+
+        private static QueryResult GetComponents(List<int> initialIndices, List<List<(int node, int weight)>> adjacencies, int l, int r)
+        {                                           
+            var components = new List<HashSet<int>>();
+            var pathCounts = new List<long>();
+            var nodeLabels = new Dictionary<int, int>();
+
+            var explored = new HashSet<int>();
             foreach (var initialIndex in initialIndices.Where(i => !explored.Contains(i)))
             {
                 var stack = new Stack<int>();
                 var prev = new Dictionary<int, (int node, int weight)>();
-
-                stack.Push(initialIndex);
+                stack.Push(initialIndex);                
+                var component = new HashSet<int>();
                 int nodeCount = 0;
+                long pathCount = 0;                
 
                 while (stack.Any())
                 {
@@ -60,25 +94,30 @@ namespace HackerRankSolutions.SuperMaximumCostQueries
                         continue;
 
                     nodeCount++;
+                    nodeLabels[currNode] = components.Count;
+                    component.Add(currNode);
                     if (prev.ContainsKey(currNode))
                     {
-                        bool isCentral = IsCentral(l, r, prev[currNode].weight);
-                        var diff = isCentral ? 0 : CountEdgesUntilCentrals(adjacencies, l, r, explored, currNode);
-                        count += nodeCount - 1 - diff;
+                        var newEdge = new Edge(prev[currNode].node, currNode, prev[currNode].weight, components.Count);                         
+                        var diff = newEdge.IsCentral(l, r) ? 0 : CountEdgesUntilCentrals(adjacencies, l, r, explored, currNode);                        
+                        pathCount += nodeCount - 1 - diff;                        
                     }                                                            
 
                     var neighbors = adjacencies[currNode].Where(t => IsAllowed(r, t.weight) && !explored.Contains(t.node)).ToList();
-                    foreach (var neighbor in neighbors)
+                    foreach (var (neighborNode, weight) in neighbors)
                     {
-                        prev[neighbor.node] = (currNode, neighbor.weight);
-                        stack.Push(neighbor.node);
+                        prev[neighborNode] = (currNode, weight);
+                        stack.Push(neighborNode);
                     }
 
                     explored.Add(currNode);
                 }
+
+                components.Add(component);
+                pathCounts.Add(pathCount);
             }
 
-            return count;
+            return new QueryResult(components, pathCounts, nodeLabels);
         }        
 
         private static int CountEdgesUntilCentrals(List<List<(int node, int weight)>> adjacencies, int l, int r, HashSet<int> explored, int initialNode)
@@ -97,10 +136,10 @@ namespace HackerRankSolutions.SuperMaximumCostQueries
 
                 countNodes++;
                 var neighborEdges = adjacencies[currNode].Where(t => IsAllowedNotCentral(l, r, t.weight) && !counted.Contains(t.node) && explored.Contains(t.node)).ToList();
-                foreach (var neighbor in neighborEdges)
+                foreach (var (neighborNode, weight) in neighborEdges)
                 {
-                    prev[neighbor.node] = (currNode, neighbor.weight);
-                    stack.Push(neighbor.node);
+                    prev[neighborNode] = (currNode, weight);
+                    stack.Push(neighborNode);
                 }
 
                 counted.Add(currNode);
