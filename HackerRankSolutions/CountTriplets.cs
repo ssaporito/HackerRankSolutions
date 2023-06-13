78c5121286255ebb6e2626796859ee14cba58a17
ssaporito <sandrosaporito@outlook.com>
2023-06-12 21:57:07 -0300

    [CountTriplets] Optimized solution

---
+++ HackerRankSolutions/CountTriplets.cs
@@ -0,0 +1,96 @@
+﻿namespace HackerRankSolutions.CountTriplets
+{
+    public class CountTriplets
+    {
+        public static long SolveCountTriplets(List<long> arr, long r)
+        {
+            var arrIndices = new Dictionary<long, List<int>>();
+            for (int i = 0; i < arr.Count; i++)
+            {
+                long v = arr[i];
+                if (!arrIndices.ContainsKey(v))
+                    arrIndices[v] = new List<int>();
+                arrIndices[v].Add(i);
+            }
+
+            long count = 0;
+            var bOptions = arrIndices.Keys.Where(b => (b % r == 0) && arrIndices.ContainsKey(b / r) && arrIndices.ContainsKey(b * r)).ToList();
+
+            foreach (var b in bOptions)
+            {
+                if (r == 1)
+                {
+                    var countTriplets = Tetrahedral((long)arrIndices[b].Count);
+                    count += countTriplets;
+                    continue;
+                }
+
+                var a = b / r;
+                var c = b * r;
+
+                foreach (var j in arrIndices[b])
+                {
+                    int aCount = CountLowerThan(arrIndices[a], 0, arrIndices[a].Count - 1, j);
+                    int cCount = CountHigherThan(arrIndices[c], 0, arrIndices[c].Count - 1, j);
+                    var tripletCount = aCount * cCount;
+                    count += tripletCount;
+                }
+            }
+
+            return count;
+        }
+
+        static int CountLowerThan(List<int> indices, int start, int end, int m)
+        {
+            if (indices[start] > m)
+                return 0;
+            if (indices[end] < m)
+                return end + 1;
+
+            int mid = start + (int)Math.Floor((end - start) / (double)2);
+            if (indices[mid] > m)
+            {
+                if (mid == 0 || indices[mid - 1] < m)
+                    return mid;
+                else
+                    return CountLowerThan(indices, start, mid, m);
+            }
+            else
+            {
+                if (mid + 1 >= indices.Count || indices[mid + 1] > m)
+                    return mid + 1;
+                else
+                    return CountLowerThan(indices, mid, end, m);
+            }
+        }
+
+        static int CountHigherThan(List<int> indices, int start, int end, int m)
+        {
+            if (indices[end] < m)
+                return 0;
+            if (indices[start] > m)
+                return indices.Count - start;
+
+            int mid = start + (int)Math.Floor((end - start) / (double)2);
+            if (indices[mid] < m)
+            {
+                if (mid + 1 == indices.Count || indices[mid + 1] > m)
+                    return indices.Count - mid - 1;
+                else
+                    return CountHigherThan(indices, mid, end, m);
+            }
+            else
+            {
+                if (mid == 0 || indices[mid - 1] < m)
+                    return indices.Count - mid;
+                else
+                    return CountHigherThan(indices, start, mid, m);
+            }
+        }
+
+        static long Tetrahedral(long n)
+        {
+            return n * (n - 1) * (n - 2) / 6;
+        }
+    }
+}
---
+++ HackerRankSolutionsTest/CountTripletsTest.cs
@@ -0,0 +1,40 @@
+﻿using HackerRankSolutions.CountTriplets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutionsTest
+{
+    public class CountTripletsTest
+    {
+        [Theory]
+        [PlainTextData(typeof(CountTripletsCaseConverter))]
+        public void TestByFiles(string arrString, long r, long expected)
+        {
+            var arr = arrString.Split(' ').ToList().Select(arrTemp => Convert.ToInt64(arrTemp)).ToList();                        
+            var actual = CountTriplets.SolveCountTriplets(arr, r);
+            Assert.Equal(expected, actual);
+        }
+
+        public class CountTripletsCaseConverter : FileToTestCaseConverter
+        {
+            public override object[] ConvertInput(StreamReader inputStream)
+            {
+                string[] nr = inputStream.ReadLine().TrimEnd().Split(' ');
+
+                long r = Convert.ToInt64(nr[1]);
+                var arrString = inputStream.ReadLine().TrimEnd();                
+
+                return new object[] { arrString, r };
+            }
+
+            public override object ConvertOutput(StreamReader outputStream)
+            {
+                long result = Convert.ToInt64(outputStream.ReadLine().TrimEnd());
+                return result;
+            }
+        }
+    }
+}
